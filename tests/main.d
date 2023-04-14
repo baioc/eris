ae793b100d3bbde0bd3b7f7c8abef2493a4f5a54
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2023-04-14 10:25:35 -0300

    Parameterize set benchmark mode (w|r|x)

--- tests/main.d
+++ tests/main.d
@@ -30,14 +30,18 @@ int unittests() {
 
 
 int benchmark(const(cstring[]) args) {
-	if (args.length < 1) return 1;
+	const n = args.length;
+	if (n < 1) return 1;
 	const cstring type = args[0];
-	if (strcmp(type, "aaset") == 0)  return benchmarkAASet(atoi(args[1]));
-	if (strcmp(type, "rbtree") == 0) return benchmarkRBTree(atoi(args[1]));
+	if (strcmp(type, "aaset") == 0 && n >= 3)  return benchmarkAASet(atoi(args[1]), args[2][0]);
+	if (strcmp(type, "rbtree") == 0 && n >= 3) return benchmarkRBTree(atoi(args[1]), args[2][0]);
 	return 1;
 }
 
-double microBenchMark(scope void delegate(const(int[])) code, uint inputSize, uint seed = 0) {
+double microBenchMark(
+	scope void delegate(const(int[]), out clock_t, out clock_t) code,
+	uint inputSize, uint seed = 0
+) {
 	int* rands = cast(int*) malloc(inputSize * int.sizeof);
 	if (inputSize != 0 && rands == null) return double.nan;
 	scope(exit) free(rands);
@@ -46,22 +50,43 @@ double microBenchMark(scope void delegate(const(int[])) code, uint inputSize, ui
 	foreach (i; 0 .. inputSize) rands[i] = rand();
 
 	int[] slice = rands[0 .. inputSize];
-	const clock_t begin = clock();
-	code(slice);
-	const clock_t end = clock();
+	clock_t begin, end;
+	code(slice, begin, end);
 
 	double elapsedMs = (end - begin) * 1e3 / CLOCKS_PER_SEC;
 	return elapsedMs;
 }
 
 
-int benchmarkAASet(int n) {
+int benchmarkAASet(int n, char mode) {
+	if (n < 0) return 1;
 	version (D_BetterC) {} else {
-		double elapsedMs = (const(int[]) inputs){
+		double elapsedMs = (const(int[]) inputs, out begin, out end){
 			import eris.set : AASet;
 			auto set = new AASet!ulong();
-			foreach (i; 0 .. n) {
-				set.upsert(inputs[i]);
+			switch (mode) {
+				case 'w': // only measure insertions
+					begin = clock();
+					foreach (i; 0 .. n) set.upsert(inputs[i]);
+					end = clock();
+					break;
+
+				case 'r': // only measure reads
+					foreach (i; 0 .. n) set.upsert(inputs[i]);
+					begin = clock();
+					foreach (i; 0 .. n) auto b = set.contains(inputs[i]);
+					end = clock();
+					break;
+
+				case 'x': // measure insert+read+remove
+					begin = clock();
+					foreach (i; 0 .. n) set.upsert(inputs[i]);
+					foreach (i; 0 .. n) auto b = set.contains(inputs[i]);
+					foreach (i; 0 .. n) set.remove(inputs[i]);
+					end = clock();
+					break;
+
+				default: assert(0);
 			}
 		}.microBenchMark(n);
 		printf("AASet: %.3f ns/element\n", elapsedMs * 1e6 / n);
@@ -69,13 +94,35 @@ int benchmarkAASet(int n) {
 	return 0;
 }
 
-int benchmarkRBTree(int n) {
+int benchmarkRBTree(int n, char mode) {
+	if (n < 0) return 1;
 	version (D_BetterC) {} else {
-		double elapsedMs = (const(int[]) inputs){
+		double elapsedMs = (const(int[]) inputs, out begin, out end){
 			import std.container.rbtree;
 			auto set = redBlackTree!ulong();
-			foreach (i; 0 .. n) {
-				set.insert(inputs[i]);
+			switch (mode) {
+				case 'w': // only measure insertions
+					begin = clock();
+					foreach (i; 0 .. n) set.insert(inputs[i]);
+					end = clock();
+					break;
+
+				case 'r': // only measure reads
+					foreach (i; 0 .. n) set.insert(inputs[i]);
+					begin = clock();
+					foreach (i; 0 .. n) auto b = inputs[i] in set;
+					end = clock();
+					break;
+
+				case 'x': // measure insert+read+remove
+					begin = clock();
+					foreach (i; 0 .. n) set.insert(inputs[i]);
+					foreach (i; 0 .. n) auto b = inputs[i] in set;
+					foreach (i; 0 .. n) set.removeKey(inputs[i]);
+					end = clock();
+					break;
+
+				default: assert(0);
 			}
 		}.microBenchMark(n);
 		printf("RedBlackTree: %.3f ns/element\n", elapsedMs * 1e6 / n);
